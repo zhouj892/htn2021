b42c18e5847c29ae6427ce51085f6a4e4dd50798
zhou Jiahui <zhou_jiahui_ca@hotmail.com>
2021-09-18 13:34:43 -0400

    progress

--- scraping/construct-url.py
+++ scraping/construct-url.py
@@ -1,5 +1,5 @@
 def constructUrl(city, province):
     rentUrl = "https://www.rentcanada.com/" + city + "-" + province
-    buyBaseUrl = "https://www.realtor.ca/" + province + "/" + city + "/real-estate"
+    buyUrl = "https://www.realtor.ca/" + province + "/" + city + "/real-estate"
 
-    return [rentUrl, buyBaseUrl]
+    return [rentUrl, buyUrl]
