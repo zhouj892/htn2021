2788679a92a946b4c1089e1de1bb812c263cae5c
Lucas <lucas.wang.to@gmail.com>
2021-09-18 18:46:43 -0400

    Fixed RentCanada scraping

--- Server/Scraping/construct-url.py
+++
@@ -1,5 +0,0 @@
-def constructUrl(city, province):
-    rentUrl = "https://www.rentcanada.com/" + city + "-" + province
-    buyUrl = "https://www.realtor.ca/" + province + "/" + city + "/real-estate"
-
-    return [rentUrl, buyUrl]
--- Server/Scraping/rentCanadaRates.py
+++ Server/Scraping/rentCanadaRates.py
@@ -1,27 +1,30 @@
 import requests
 
-def getCityId(city, province):
+
+def RentcanadaPrice(city: str, province: str):
+    cityId = getCityId(city, province)
+    return getRate(cityId)
+
+
+def getCityId(city: str, province: str):
     url = "https://www.rentcanada.com/api/routing?pageUrl=%2F" + city + "-" + province
     result = requests.get(url).json()
     cityId = result['pageData']['objects']['id']
     return cityId
-    
 
-def getRate(cityId):
-    rates = {}
+
+def getRate(cityId: int):
+    rates = []
     url = "https://www.rentcanada.com/api/listings?cityId="+str(cityId)
     result = requests.get(url).json()
     allListings = result['listings']
-    listingsLedger = result['listingsLedger']
 
     for listing in allListings:
-        listingId = listing["id"]
-        maxRate = listing["maxRate"]
-        minRate = listing["minRate"]
-        avgRate = (int(maxRate) + int(minRate))/2
-        rates[listingId] = avgRate
+        maxRate = int(listing["maxRate"])
+        minRate = int(listing["minRate"])
+        if maxRate == minRate:
+            rates.append(maxRate)
+        else:
+            rates.append(minRate)
+            rates.append(maxRate)
     return(rates)
-        
-
-
-
